34df3ad0311cfca17f59dcbbbc8d63d2b3b43c22
dani campdelacreu <122914770+danicampdelacreu@users.noreply.github.com>
2023-09-27 10:03:16 +0200

    explicaciones codigo

--- react_vite/src/routes/campus/Campus.jsx
+++ react_vite/src/routes/campus/Campus.jsx
@@ -24,26 +24,27 @@ export default function Campus() {
     setAutoplaySpeed(3000); // Establecer la velocidad deseada después de 0 segundos
   }, []);
 
+  // Configuración para un carrusel de imágenes
   const settings = {
-    dots: true,
-    infinite: true,
-    speed: 3000,
-    autoplay: true,
-    autoplaySpeed: autoplaySpeed,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    dots: true,             // Muestra puntos de navegación
+    infinite: true,         // Permite desplazarse infinitamente
+    speed: 3000,            // Velocidad de cambio de imágenes (3 segundos)
+    autoplay: true,         // Reproduce automáticamente las imágenes
+    autoplaySpeed: autoplaySpeed, // Usa la velocidad que configuramos anteriormente
+    slidesToShow: 1,        // Muestra una imagen a la vez
+    slidesToScroll: 1,      // Se desplaza una imagen a la vez
   };
 
   return (
     <div className="campus-general">
       <h1>Campus</h1>
       <div className="campus-container">
-        <Slider {...settings}>
-          {imageUrls.map((url, index) => (
-            <div key={index} className="campus-slide">
-              <img
+        <Slider {...settings}> 
+          {imageUrls.map((url, index) => (             // mapea imageUrls por cada imagen se ejecuta una funcion
+            <div key={index} className="campus-slide"> 
+              <img                                     // creamos un div dentro de cada imageUrls
                 src={url}
-                alt={`Imagen ${index + 1}`}
+                alt={`Imagen ${index + 1}`}            // dentro div se coloca un elemento img 
                 className="campus-image"
               />
             </div>
--- react_vite/src/routes/contacto/Contacto.jsx
+++ react_vite/src/routes/contacto/Contacto.jsx
@@ -4,49 +4,49 @@ import emailjs from '@emailjs/browser';
 import './Contacto.css';
 
 export default function Contacto() {
-  const form = useRef();
+  const form = useRef(); // creamos referencia REF para acceder al elemento formulario del Dom mas abajo
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = (e) => { // funcion que se ejecuta cuando se envia formulario
+    e.preventDefault();      // previene comportamiento predeterminado de formulario
 
-    const formData = new FormData(form.current);
+    const formData = new FormData(form.current); // recopilamos datos formulario
 
     if (formData.get('user_name') && formData.get('user_email') && formData.get('message')) {
       emailjs
         .sendForm('service_sjyf30z', 'template_itkkk5i', form.current, 'ybVHXbi9wadjWsNQj')
-        .then(
-          (result) => {
+        .then( //utilizamos promes para manejar respueste exsito servidor
+          (result) => { // si envio es correcto actua result
             console.log(result.text);
             alert('¡Mensaje enviado correctamente! Nos pondremos en contacto con usted ');
-            window.location.reload();
+            window.location.reload(); // recargar pagina para borrar lo escrito
           },
-          (error) => {
+          (error) => { // si tenemos un error de servidor actua error
             console.log(error.text);
             alert('Ocurrió un error al enviar el mensaje. Inténtalo nuevamente más tarde.');
           }
         );
-    } else {
+    } else { // en caso contraro al if salta el alert
       alert('Por favor, rellena todos los campos.');
     }
   };
 
   return (
     <div className="contacto">
-      <form ref={form} onSubmit={sendEmail}>
-        <div className="contacto-name">
+      <form ref={form} onSubmit={sendEmail}>        {/*  ref={form} --Define formulario HTML permite acceder formulario dede codigo JS*/}
+        <div className="contacto-name">             {/*  onSubmit={sendEmail} cuando usuario haga click "enviar" ejecuta la funcion 'sendEmail */}
           <label>Nom</label>
-          <input type="text" name="user_name" />
+          <input type="text" name="user_name" />    {/*  input tipo text */}
         </div>
         <div className="contacto-email">
           <label>Email</label>
-          <input type="email" name="user_email" />
+          <input type="email" name="user_email" />  {/*  input tipo email */}
         </div>
         <div className="contacto-message">
           <label>Missatge</label>
           <textarea name="message" />
         </div>
         <div className="contacto-submit">
-          <input type="submit" value="Enviar" />
+          <input type="submit" value="Enviar" />    {/*  input tipo submit */}
         </div>
       </form>
     </div>
